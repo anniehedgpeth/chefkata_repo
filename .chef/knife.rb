f63049829df5da063fc7a85efd605d8d7dd5b107
anniehedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-19 14:50:16 -0600

    data bags

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "annie_hedgpeth"
+client_key               "#{current_dir}/annie_hedgpeth.pem"
+chef_server_url          "https://api.chef.io/organizations/practice-org-8"
+cookbook_path            ["#{current_dir}/../cookbooks"]
